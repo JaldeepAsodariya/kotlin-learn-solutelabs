af01b5840d5eff4d19fa849ae34a16ee7116fddc
Jaldeep Asodariya <jdpatel.android@gmail.com>
2019-03-06 13:27:34 +0530

    Added 02-String Templates

---
+++ src/02StringTemplates.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var value = "This is a String"
+
+    println("The lenght of '$value' is ${value.length}")
+
+    print(""" This is
+    Multiline
+    code """)
+}
