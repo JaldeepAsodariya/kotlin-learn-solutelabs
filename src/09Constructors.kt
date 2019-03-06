0af10646d01831ab861640b642fe9d1aee7816b3
Jaldeep Asodariya <jdpatel.android@gmail.com>
2019-03-06 18:26:08 +0530

    Added 09-Constructors

---
+++ src/09Constructors.kt
@@ -0,0 +1,9 @@
+class ConstructorDemo(arg1: Int, arg2: String) {
+    init {
+        // Code for Primary constructor
+    }
+
+    constructor(arg1: Int, arg2: String, arg3: String): this(arg1, arg2) {
+        // Code for Secondary constructor
+    }
+}
